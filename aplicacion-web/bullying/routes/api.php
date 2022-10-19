6e42eba725d92bc9dc30d7f76ee0a60e0d98097b
J. Luis Vargas <112353437+JLuisHub@users.noreply.github.com>
2022-10-19 11:52:42 -0500

    Revert "API Alumnos-reportar"

--- aplicacion-web/bullying/routes/api.php
+++ aplicacion-web/bullying/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReporteController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,12 +16,4 @@ use App\Http\Controllers\ReporteController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-Route::apiResource('reportes', ReporteController::class);
-
-Route::group(["auth:sanctum"],function(){
-    Route::get("reportes_guardar","App\Http\Controllers\ReporteController@store");
-    Route::get("reportes_eliminar/{id_reporte}","App\Http\Controllers\ReporteController@destroy");
 });
