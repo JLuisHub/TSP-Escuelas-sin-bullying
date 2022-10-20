e5477583e056796fc1e60b2f410495a7327ff632
Jose Luis Vargas Ibarra <vargasibarra01@gmail.com>
2022-10-20 18:57:33 -0500

    Codigo fuerte app-movil

--- app-movil/EscuelaSinBullying/components/ReportCard.js
+++ app-movil/EscuelaSinBullying/components/ReportCard.js
@@ -1,25 +1,56 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const ReportCard = ({desc, date}) => {
-  return (
-    <View style = {styles.cardCont}>
-        <View style = {styles.subCardCont}>
-            <Text style = {styles.infoHeader}> Fecha: </Text>
-            <Text style = {styles.info}> {date} </Text>
+const ReportCard = ({id, desc, date}) => {
+
+    const onDeletePress = () => {
+        console.log("Se elimina el reporte con id: " + id)
+    }
+    
+    return (
+        <View style = {styles.cardCont}>
+
+            <View style = {styles.subCardCont}>
+
+                <Text style = {styles.infoHeader}>
+                    Fecha: 
+                </Text>
+
+                <Text style = {styles.info}>
+                    {date}
+                </Text>
+
+            </View>
+
+            <View style = {styles.subCardCont}>
+
+                <View style = {{flex:4}}>
+                    <Text style = {styles.infoHeader}>
+                        Descripcion:
+                    </Text>
+
+                    <Text style = {styles.info}>
+                        {desc}
+                    </Text>
+                </View>
+
+                <View style = {styles.buttonCont}>
+                    <CustomButton 
+                        text = 'Eliminar'
+                        onPress = {onDeletePress}
+                    />
+                </View>
+                
+            </View>
         </View>
-        <View >
-            <Text style = {styles.infoHeader}> Descripcion: </Text>
-            <Text style = {styles.info}> {desc} </Text>
-        </View>
-    </View>
-  )
+    )
 }
 
 const styles = StyleSheet.create({
     cardCont: {
-        padding: 3,
-        marginVertical: 10
+        //padding: 3,
+        marginVertical: 20
     },
 
     subCardCont: {
@@ -34,6 +65,12 @@ const styles = StyleSheet.create({
 
     info: {
         fontSize: 20,
+        flex: 1,
+    },
+
+    buttonCont: {
+        alignSelf: 'flex-end',
+        flex: 2
     }
 })
 
